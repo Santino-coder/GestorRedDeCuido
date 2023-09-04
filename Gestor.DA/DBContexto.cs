52d4bcdcb5cc5355097666b73952dc7eb569e1fa
Santino Cortés <61328413+Santino-coder@users.noreply.github.com>
2023-09-03 23:38:29 -0600

    Add project files.

---
+++ Gestor.DA/DBContexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestor.DA
+{
+    public class DBContexto : DbContext
+    {
+        public DBContexto(DbContextOptions<DBContexto> opciones) : base(opciones) { }
+    }
+}
