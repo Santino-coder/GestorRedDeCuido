10789b97a8431e895ae770f218a2d2a2d4f5c585
Cristel Prendas <criismpg@gmail.com>
2023-09-04 19:30:41 -0600

    ultimo

--- Gestor.UI/Program.cs
+++ Gestor.UI/Program.cs
@@ -1,77 +1,27 @@
-using Gestor.DA;
-using Gestor.UI.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using System.Configuration;
-using Microsoft.Extensions.Configuration;
-using Gestor.BS;
+using Gestor.UI;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-
-/*void ConfigureServices(IServiceCollection services)
+namespace Gestor.UI
 {
-   // Configuración de la conexión a la base de datos
-   IConfiguration configuration = new ConfigurationBuilder()
-       .SetBasePath(Directory.GetCurrentDirectory())
-       .AddJsonFile("appsettings.json") // Asegúrate de que el nombre del archivo sea correcto
-       .Build();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-   services.AddDbContext<DBContexto>(options =>
-   {
-       options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
-   });
-
-   // Otras configuraciones y servicios
-}*/
-IConfiguration Configuration;
-
-void ConfigureServices(IServiceCollection services)
-{
-    services.AddDbContext<DBContexto>(opciones => opciones.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-    services.AddControllersWithViews();
-    services.AddScoped<IServiciosRedDeCuido, ServiciosRedDeCuido>();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
-
-app.Run();
