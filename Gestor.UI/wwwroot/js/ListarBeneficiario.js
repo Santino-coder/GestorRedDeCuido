0e3a03ad437a413c891180cebf4792d88f86bced
Santino Cortés <61328413+Santino-coder@users.noreply.github.com>
2023-09-06 11:07:01 -0600

    Buscar de ListarBeneficiario (falta filtrar por estado)

--- Gestor.UI/wwwroot/js/ListarBeneficiario.js
+++ Gestor.UI/wwwroot/js/ListarBeneficiario.js
@@ -1,10 +1,27 @@
-﻿// Filter table
+﻿$(document).ready(function () {
+    $(".search").keyup(function () {
+        var searchTerm = $(".search").val();
+        var listItem = $('.results tbody').children('tr');
+        var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
 
-$(document).ready(function(){
-  $("#tableSearch").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#myTable tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        $.extend($.expr[':'], {
+            'containsi': function (elem, i, match, array) {
+                return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+            }
+        });
+
+        $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function (e) {
+            $(this).attr('visible', 'false');
+        });
+
+        $(".results tbody tr:containsi('" + searchSplit + "')").each(function (e) {
+            $(this).attr('visible', 'true');
+        });
+
+        var jobCount = $('.results tbody tr[visible="true"]').length;
+        $('.counter').text(jobCount + ' item');
+
+        if (jobCount == '0') { $('.no-result').show(); }
+        else { $('.no-result').hide(); }
     });
-  });
 });
