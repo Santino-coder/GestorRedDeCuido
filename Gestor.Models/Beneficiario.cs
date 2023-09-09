2dd13d3eb399ecee2a93e1f6de230e9c5972a49b
Santino Cortés <61328413+Santino-coder@users.noreply.github.com>
2023-09-09 17:03:01 -0600

    Ya funciona el alternativa de beneficiario pero se puede mejorar.

--- Gestor.Models/Beneficiario.cs
+++ Gestor.Models/Beneficiario.cs
@@ -13,36 +13,63 @@ namespace Gestor.Models
         [Key]
         public int idBeneficiario { get; set; }
 
-        public int DNI { get; set; }
+        [Required(ErrorMessage = "Por favor, digite el número de identificación.")]
+        public int? DNI { get; set; }
 
-        public string? Nombre { get; set; }
+        [Required(ErrorMessage = "Por favor, ingrese el nombre.")]
+        public string Nombre { get; set; }
 
-        public string? Apellidos { get; set; }
+        [Required(ErrorMessage = "Por favor, ingrese los apellidos.")]
+        public string Apellidos { get; set; }
 
+        [Required(ErrorMessage = "Por favor, seleccione una fecha de nacimiento.")]
         public DateTime FechaDeNacimiento { get; set; }
 
-        public int Edad { get; set; }
+        [Range(0, 100, ErrorMessage = "La edad debe estar entre 18 y 100.")]
+        public int? Edad { get; set; }
 
-        public string? EstadoCivil { get; set; }
 
-        public string? Distrito { get; set; }
+        [Required(ErrorMessage = "Por favor, seleccione el estado civil.")]
+        public string EstadoCivil { get; set; }
 
-        public string? Direccion { get; set; }
+        [Required(ErrorMessage = "Por favor, seleccione el distrito en el que se encuantra la residencia.")]
+        public string Distrito { get; set; }
 
-        public int Telefono { get; set; }
+        [Required(ErrorMessage = "Por favor, ingrese la dirección completa.")]
+        public string Direccion { get; set; }
 
-        public string? IngresoEconomicoYOrigen { get; set; }
 
+        [Required(ErrorMessage = "Por favor, digite el número de teléfono.")]
+        public int? Telefono { get; set; }
+
+        [Required(ErrorMessage = "Por favor, digite el ingreso económico mensual y su origen.")]
+        public string IngresoEconomicoYOrigen { get; set; }
+
+        [Required(ErrorMessage = "Por favor, seleccione la fecha de ingreso.")]
         public DateTime FechaIngreso { get; set; }
 
-        public string? Estado {  get; set; }
+        [Required(ErrorMessage = "Por favor, seleccione el estado")]
+        public string Estado { get; set; }
 
-        public string? PersonaEncargada { get; set; }
+        [Required(ErrorMessage = "Por favor, ingrese el nombre de la persona encargada.")]
+        public string PersonaEncargada { get; set; }
 
+        [Required(ErrorMessage = "Por favor, digite el número de teléfono de la persona encargada.")]
         public int NumeroPersonaEncargada { get; set; }
 
         public string? Observaciones { get; set; }
 
-        public string? Alternativa { get; set; }
+        [Required(ErrorMessage = "Debe elegir almenos una alternativa")]
+        public string Alternativa1 { get; set; }
+
+        public string? Alternativa2 { get; set; }
+
+        public string? Alternativa3 { get; set; }
+
+        public string? Alternativa4 { get; set; }
+
+        public string? Alternativa5 { get; set; }
+
     }
+
 }
--- Gestor.UI/wwwroot/js/AgregarBeneficiario.js
+++ Gestor.UI/wwwroot/js/AgregarBeneficiario.js
@@ -1,1 +1,7 @@
-﻿
+﻿$(document).ready(function () {
+    $(".column-to-hide").each(function () {
+        if ($(this).text().trim() === "") {
+            $(this).closest("tr").find(".column-to-hide").addClass("hide-column");
+        }
+    });
+});
