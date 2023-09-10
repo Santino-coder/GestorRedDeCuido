01583769d6c255135cd4ca38ae1290b307bda677
Santino Cortés <61328413+Santino-coder@users.noreply.github.com>
2023-09-09 21:58:47 -0600

    Más estilo

--- Gestor.UI/wwwroot/js/AgregarBeneficiario.js
+++ Gestor.UI/wwwroot/js/AgregarBeneficiario.js
@@ -1,1 +1,21 @@
-﻿
+﻿$(document).ready(function () {
+    $("#mostrarAlternativa2").click(function () {
+        $("#Alternativa2").show(); // Mostrar el div al hacer clic en el botón
+        $("#mostrarAlternativa2").hide();
+    });
+
+    $("#mostrarAlternativa3").click(function () {
+        $("#Alternativa3").show(); // Mostrar el div al hacer clic en el botón
+        $("#mostrarAlternativa3").hide();
+    });
+
+    $("#mostrarAlternativa4").click(function () {
+        $("#Alternativa4").show(); // Mostrar el div al hacer clic en el botón
+        $("#mostrarAlternativa4").hide();
+    });
+
+    $("#mostrarAlternativa5").click(function () {
+        $("#Alternativa5").show(); // Mostrar el div al hacer clic en el botón
+        $("#mostrarAlternativa5").hide();
+    });
+});
