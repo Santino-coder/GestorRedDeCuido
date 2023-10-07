6dcdb6a666fe13c76f773164a3ec7aa4384fddbf
Santino Cortes <santino.cortes01@gmail.com>
2023-10-06 21:00:00 -0600

    Ordenar alfaabéticamente por nombre en la vista de listar beneficiarios

--- Gestor.UI/wwwroot/js/ListarBeneficiario.js
+++ Gestor.UI/wwwroot/js/ListarBeneficiario.js
@@ -25,3 +25,27 @@
         else { $('.no-result').hide(); }
     });
 });
+
+
+
+document.getElementById('miBotonOrdenar').addEventListener('click', function () {
+    var table = document.getElementById('miTabla');
+    var rows = Array.from(table.querySelectorAll('tr')); // Obtienen todas las filas de la tabla
+    var header = rows.shift(); // Elimina el encabezado de la tabla
+
+
+    rows.sort(function (a, b) {
+        var nameA = a.querySelector('td:nth-child(2)').textContent.trim().toLowerCase(); // Asume que la columna del nombre es la segunda 
+        var nameB = b.querySelector('td:nth-child(2)').textContent.trim().toLowerCase();
+
+        return nameA.localeCompare(nameB); // Compara los nombres de forma insensible a mayúsculas y minúsculas
+    });
+
+    // Vuelve a agregar las filas ordenadas a la tabla
+    rows.forEach(function (row) {
+        table.appendChild(row);
+    });
+
+    // Vuelve a agregar el encabezado
+    table.insertBefore(header, table.firstChild);
+});
