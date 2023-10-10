4f896f20fd7354286c88e27e5ea54cba618eaa04
Santino Cortes <santino.cortes01@gmail.com>
2023-10-09 22:57:59 -0600

    Lista la funcionalidad de descargar detalles de beneficiario en formato PDF

---
+++ Gestor.UI/wwwroot/js/PDF.js
@@ -0,0 +1,9 @@
+﻿function generatePDF() {
+    var doc = new jsPDF();
+    doc.fromHTML(document.getElementById("html2pdf"), 10, 10, {
+        'width': 100
+    },
+        function (a) {
+            doc.save("DetallesDeBeneficiario.pdf");
+        });
+}
