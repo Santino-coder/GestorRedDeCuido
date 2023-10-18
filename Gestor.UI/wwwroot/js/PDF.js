a0f08c1291104ab7394891110859bfc2df092b5a
Santino Cortes <santino.cortes01@gmail.com>
2023-10-17 20:09:09 -0600

    Ya se conservan los saltos de linea al generar el PDF.

--- Gestor.UI/wwwroot/js/PDF.js
+++ Gestor.UI/wwwroot/js/PDF.js
@@ -1,10 +1,16 @@
 ﻿// Función para generar un archivo PDF a partir de contenido HTML
 function generatePDF() {
-    var doc = new jsPDF(); // Crea una instancia de jsPDF para generar el PDF
+    var doc = new jsPDF({
+        orientation: "portrait",
+        unit: "mm",
+        format: "a4",
+        lineHeight: 1.5
+    });
 
     // Convierte el contenido HTML del elemento con el id "html2pdf" en un archivo PDF
     doc.fromHTML(document.getElementById("html2pdf"), 10, 10, {
-        'width': 100 // Ancho del área del contenido
+        'width': 190, // Ancho del área del contenido
+        'whitePage': true // Conservar saltos de línea
     }, function (output) {
         // Guarda el PDF generado con el nombre "DetallesDeBeneficiario.pdf"
         doc.save("DetallesDeBeneficiario.pdf");
